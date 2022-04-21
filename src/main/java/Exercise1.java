90e4cbb58e9957909c7cf1bc06b34e6b51dc832e
Connor Shillady <c.shillady24@gmail.com>
2022-04-21 14:17:50 -0400

    Exercise 1

---
+++ src/main/java/Exercise1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Exercise1 {
+
+    public static void main(String[] args){
+
+        //Write a Java program that prompts the user to enter their name, and returns their name in upper case.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your name.");
+        String sentence = scanner.nextLine();
+        System.out.println(sentence.toUpperCase());
+    }
+}
